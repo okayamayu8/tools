4d350fcdcfbc09e38b9d81b03346dcf69ad08315
NAGANO Yuya <naganoy@cw01.cc.kek.jp>
2025-03-21 12:03:11 +0900

    add function

--- python/.ipynb_checkpoints/tools-checkpoint.py
+++ python/.ipynb_checkpoints/tools-checkpoint.py
@@ -27,3 +27,14 @@ def truncate_alm(alm, lmax_in, lmax_out):
         alm_new[2,idx_start_out:idx_stop_out+1] = alm[2,idx_start_in:idx_stop_in+1]
     return alm_new
 
+def gaussian_sidelobe(nside, fwhm, peak):
+    sigma = fwhm/(2*np.sqrt(2*np.log(2)))
+    npix = hp.nside2npix(nside)
+    maps = np.zeros((3,npix), np.float64)
+    pixs = np.linspace(0,npix-1,npix)
+    theta,phi=hp.pix2ang(nside, np.int32(pixs))
+    result = 1/(2*np.pi*sigma**2)*np.exp(-(theta - peak)**2/sigma**2)
+    maps[0] = result
+    maps[1] = result*np.cos(2*phi)
+    maps[2] = -result*np.sin(2*phi)
+    return maps
